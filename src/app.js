231bc5693f2303d8385dba091bd5ac35f0d307b5
Domingo Ramírez Morales <128168900+Rzmsdo@users.noreply.github.com>
2024-11-15 22:35:18 +0000

    aqui esta

--- src/app.js
+++ src/app.js
@@ -9,3 +9,24 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
+
+let final = "";
+
+function elige(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+final =
+  elige(who) + " " + elige(action) + " " + elige(what) + " " + elige(when);
+
+document.getElementById("excusagenerada").innerText = final;
